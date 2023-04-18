0d9d979317f9d91c8fa91ce6a2be00a67fda1678
vincent <vin_wenzhi@163.com>
2023-04-18 14:38:39 +0800

    Evaluate the lane lines

--- lane_evaluation.py
+++ lane_evaluation.py
@@ -48,6 +48,9 @@ def eval_lanes(ds_dir, gt_dir, save_dir, m):
             point_tem = (item.get("x"), item.get("z"))
             ds_line_tem.append(point_tem)
         ds_lines.append(ds_line_tem)
+        #对每一条车道线按照z坐标值进行降序排序
+    for line_a in ds_lines:
+        line_a.sort(reverse=True, key = lambda x: x[1])
 
     for gt_key, gt_value in gt_json.items():
         gt_line_tem = []
@@ -55,29 +58,35 @@ def eval_lanes(ds_dir, gt_dir, save_dir, m):
             point_tem_gt = (gt_item.get("x"), gt_item.get("z"))
             gt_line_tem.append(point_tem_gt)
         gt_lines.append(gt_line_tem)
+        #对每一条车道线按照z坐标值进行降序排序
+    for line_b in gt_lines:
+        line_b.sort(reverse=True, key = lambda x: x[1])
+    #print("ds_lines:  ", ds_lines)
+    #print("gt_lines:  ", gt_lines)
 
     ds_lines_func = fit_curve(ds_lines, m)
     gt_lines_func = fit_curve(gt_lines, m)
     #print("ds_lines_func: ", ds_lines_func)
     #print("gt_lines_func ", gt_lines_func)
     for i in range(len(ds_lines_func)):
+
         #依次处理每一条车道线
-
-        #ds_x_min = min(ds_lines[i])[0]
-        #ds_x_max = max(ds_lines[i])[0]
-
-        #gt_x_min = min(gt_lines[i])[0]
-        #gt_x_max = max(gt_lines[i])[0]
-
         ds_z_min = ds_lines[i][-1][1]
+        #print('ds_z_min: ', ds_z_min)
         ds_z_max = ds_lines[i][0][1]
+        #print("ds_z_max: ", ds_z_max)
         gt_z_min = gt_lines[i][-1][1]
+        #print("gt_z_min: ", gt_z_min)
         gt_z_max = gt_lines[i][0][1]
+        #print("gt_z_max: ", gt_z_max)
 
         if (gt_z_max <= ds_z_min) or (gt_z_min >= ds_z_max):
-            #print("Warning: 采样不规范！！")
-            dist_temp = float('inf')
+            print("Warning: 采样不规范！！")
+            
             dist = []
+            
+            #print("ds_z_min: ", ds_z_min)
+            #print("ds_z_max: ", ds_z_max)
             z_interval = np.arange(ds_z_min, ds_z_max, 0.1)
             #print("z_interval:  ", z_interval)
             x_ds = ds_lines_func[i](z_interval)
@@ -85,6 +94,7 @@ def eval_lanes(ds_dir, gt_dir, save_dir, m):
             #print("x_ds: ", x_ds)
             #print("x_gt: ", x_gt)
             for j in range(len(z_interval)):
+                dist_temp = float('inf')
                 for k in range(len(z_interval)):
                     ds_point = [x_ds[j], z_interval[j]]
                     gt_point = [x_gt[k], z_interval[k]]
@@ -95,29 +105,37 @@ def eval_lanes(ds_dir, gt_dir, save_dir, m):
                 dist.append(dist_temp)
             #print("dist: ", dist)
         else:
-            dist_temp = float('inf')
             dist = []
             z_min = max(ds_z_min, gt_z_min)
             z_max = min(ds_z_max, gt_z_max)
             z_interval = np.arange(z_min, z_max, 0.1)
             #print("z_interval: ", z_interval)
+            #print("len of z_interval: ", len(z_interval))
             x_ds = ds_lines_func[i](z_interval)
             x_gt = gt_lines_func[i](z_interval)
+            #print("len of x_ds: ", len(x_ds))
+            #print("len of x_gt: ", len(x_gt))
             #print("x_ds: ", x_ds)
             #print("x_gt: ", x_gt)
             for j in range(len(z_interval)):
+                dist_temp = float('inf')
+                #print(j)
                 for k in range(len(z_interval)):
-                    ds_point = [ x_ds[j], z_interval[j]]
+                    ds_point = [x_ds[j],  z_interval[j]]
                     gt_point = [x_gt[k],  z_interval[k]]
                     point_dist = calc_point_distance(ds_point, gt_point)
+                    #print("point_dist: ", point_dist)
                     if point_dist < dist_temp:
                         dist_temp = point_dist
+                    #print("dist_temp: ", dist_temp)
 
                 dist.append(dist_temp)
+                #print("dist:  ", dist)
+            #print("len of dist: ", len(dist))
             #print("dist: ", dist)
 
         # 绘制图像
-        plt.figure()
+        plt.figure(figsize=(16, 9))
         plt.plot(z_interval, x_gt, 'r', label='ground truth')
         plt.plot(z_interval, x_ds, 'b', label='predicted values')
         plt.xlabel("Z coordinate value(m)")
@@ -125,32 +143,18 @@ def eval_lanes(ds_dir, gt_dir, save_dir, m):
         plt.title("Lane {} Comparison".format(i))
         plt.legend()
 
-        plt.savefig(save_dir + "lane{}_evaluation.jpg".format(i), dpi=600)
-
+        #plt.savefig(save_dir + "baiozhu_lane{}_evaluation.jpg".format(i), dpi=600)
+        plt.show()
         print("车道线{}的精度(m): ".format(i), sum(dist) / len(dist))
 
-        plt.show()
+        
 if __name__ == '__main__':
-    save_dir =r'F:\三维重建(毫末公司实习）\scenerf_result_json\00046'
-    pred_path = r'F:\三维重建(毫末公司实习）\scenerf_result_json\00046\lane_ds_00046.json'
-    gt_path = r'F:\三维重建(毫末公司实习）\scenerf_result_json\00046\lane_gt_00046.json'
-    eval_lanes(pred_path, gt_path, save_dir, 4)
+    save_dir ='/root/vincent/Scenerf/scenerf_result_json/00046/'
+    pred_path = '/root/vincent/Scenerf/scenerf_result_json/00046/lane_ds_00046.json'
+    #gt_path = '/root/vincent/Scenerf/scenerf_result_json/00046/lane_gt_00046.json'
+    gt_path = '/root/vincent/Scenerf/scenerf_result_json/00046/lane_00046_biaozhu.json'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    #雷达点云做参考进行评估时，由于数据量比较多，可以选择高阶的多项式进行拟合(例如：4)。
+    #而当采用人工标注作为参考进行评估时，由于数据量比较少，并且由于车道线大部分情况下可近似为直线，因此应该选择低阶的多项式进行拟合(例如：1)。
+    eval_lanes(pred_path, gt_path, save_dir, 1)     
 
